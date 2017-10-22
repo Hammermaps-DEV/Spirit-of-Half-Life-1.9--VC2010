d875c38a3b04ce05979177a5eb7666609a197b19
Lucas <godkiller_nt@hammermaps.de>
2017-10-22 20:21:14 +0200

    Fix wrong check

--- dlls/cbase.cpp
+++ dlls/cbase.cpp
@@ -884,7 +884,7 @@ int CBaseEntity :: TakeDamage( entvars_t* pevInflictor, entvars_t* pevAttacker, 
 	// (that is, no actual entity projectile was involved in the attack so use the shooter's origin). 
 	if ( pevAttacker == pevInflictor )	
 	{
-		vecTemp = pevInflictor->origin - ( VecBModelOrigin(pev) );
+		vecTemp = pevAttacker->origin - ( VecBModelOrigin(pev) );
 	}
 	else
 	// an actual missile was involved.
