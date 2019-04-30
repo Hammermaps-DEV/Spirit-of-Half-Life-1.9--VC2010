f82508be5108892f44c67a976e16ed9ead650a9d
Lucas <godkiller_nt@hammermaps.de>
2019-04-30 21:41:04 +0200

    More responsive status bar (targeted players names, HP, AP

--- dlls/player.cpp
+++ dlls/player.cpp
@@ -4516,7 +4516,7 @@ void CBasePlayer :: UpdateClientData( void )
 	if ( m_flNextSBarUpdateTime < gpGlobals->time )
 	{
 		UpdateStatusBar();
-		m_flNextSBarUpdateTime = gpGlobals->time + 0.2;
+		m_flNextSBarUpdateTime = gpGlobals->time + 0.01;
 	}
 
 	// Send flashlight status
