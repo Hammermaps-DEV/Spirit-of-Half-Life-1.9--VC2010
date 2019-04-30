591e04ddbe20eb6dc024bb6152c2f5b0c6740302
Lucas <godkiller_nt@hammermaps.de>
2019-05-01 00:02:28 +0200

    Remove languages filter code

--- dlls/util.cpp
+++ dlls/util.cpp
@@ -1666,10 +1666,9 @@ void UTIL_BloodStream( const Vector &origin, const Vector &direction, int color,
 	if ( !UTIL_ShouldShowBlood( color ) )
 		return;
 
-	if ( g_Language == LANGUAGE_GERMAN && color == BLOOD_COLOR_RED )
+	if (color == BLOOD_COLOR_RED )
 		color = 0;
 
-	
 	MESSAGE_BEGIN( MSG_PVS, SVC_TEMPENTITY, origin );
 		WRITE_BYTE( TE_BLOODSTREAM );
 		WRITE_COORD( origin.x );
@@ -1691,7 +1690,7 @@ void UTIL_BloodDrips( const Vector &origin, const Vector &direction, int color, 
 	if ( color == DONT_BLEED || amount == 0 )
 		return;
 
-	if ( g_Language == LANGUAGE_GERMAN && color == BLOOD_COLOR_RED )
+	if (color == BLOOD_COLOR_RED )
 		color = 0;
 
 	if ( g_pGameRules->IsMultiplayer() )
