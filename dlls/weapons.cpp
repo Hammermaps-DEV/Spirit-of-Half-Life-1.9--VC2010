0dc080646e5f6c79d23cd9386d9ffe67b1e1b53c
Lucas <godkiller_nt@hammermaps.de>
2019-04-30 22:59:21 +0200

    Added precaching of item_healthkit and ammo_9mmbox on world start

--- dlls/weapons.cpp
+++ dlls/weapons.cpp
@@ -405,6 +405,7 @@ void W_Precache(void)
 
 	// common world objects
 	UTIL_PrecacheOther( "item_suit" );
+	UTIL_PrecacheOther( "item_healthkit" );
 	UTIL_PrecacheOther( "item_battery" );
 	UTIL_PrecacheOther( "item_antidote" );
 	UTIL_PrecacheOther( "item_security" );
@@ -426,6 +427,7 @@ void W_Precache(void)
 	UTIL_PrecacheOtherWeapon( "weapon_9mmAR" );
 	UTIL_PrecacheOther( "ammo_9mmAR" );
 	UTIL_PrecacheOther( "ammo_ARgrenades" );
+	UTIL_PrecacheOther( "ammo_9mmbox" );
 
 	// python
 	UTIL_PrecacheOtherWeapon( "weapon_357" );
